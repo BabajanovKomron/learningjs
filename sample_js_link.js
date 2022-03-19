60d6afa7f954499cd93fa9817984e0f71ad2d625
Komron <KomronBabajanov0747@gmail.com>
2022-03-19 22:50:50 +0500

    Update sample_js_link.js

--- sample_js_link.js
+++ sample_js_link.js
@@ -1,4 +1,4 @@
 console.log("script2");
-var paragraph = document.createElement("p");
+var paragraph = document.createElement("button");
 paragraph.innerHTML = "test";
 document.body.appendChild(paragraph);
