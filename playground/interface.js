ea99dfcc4e5c6dcafc42cef64852cca07b2c3c98
AmirjohnSanhinov <amir.sanhinov@infotec.ua>
2022-03-29 14:09:41 +0300

    added playground folder

---
+++ playground/interface.js
@@ -0,0 +1,13 @@
+var fileSelector = document.createElement("textarea");
+	fileSelector.value = "graphics";
+document.body.appendChild(fileSelector);
+
+var button = document.createElement("button");
+	button.innerText = "LOAD JS FILE!";
+	button.addEventListener("click", function() {
+			var script = document.createElement("script");
+			script.src = fileSelector.value.match(/(\w+)(\..*)?/)[1] + ".js";
+			document.body.appendChild(script);
+		}
+	);
+document.body.appendChild(button);
